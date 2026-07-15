c54e26de90950180de8fce32eff2297ab20800d3
uncedev <jhonbrian@uncle-dev.com>
2026-07-15 23:55:18 +0800

    feat: update API_URL in AdminSignup and Signup components to use backend_server variable

--- frontend/src/CLIENT/signup/Signup.jsx
+++ frontend/src/CLIENT/signup/Signup.jsx
@@ -4,9 +4,10 @@ import './signup.css'
 import { Link, useNavigate } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast'
 import { BsEye, BsEyeSlash } from 'react-icons/bs'
+import { backend_server } from '../../main'
 
 const Signup = () => {
-  const API_URL = 'http://localhost:5000/api/v1/signup'
+  const API_URL = `${backend_server}/api/v1/signup`
 
   const refUsername = useRef(null)
 
